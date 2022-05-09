c6c128c6c4fce3025c980a7983613722a991904a
Pavel <pavelguest@yandex.ru>
2022-05-09 11:14:01 +0300

    init: start task

---
+++ project-management-app/src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { setupStore } from './redux/store';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const store = setupStore();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
