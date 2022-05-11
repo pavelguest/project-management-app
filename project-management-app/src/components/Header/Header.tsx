0ef83ef7807d470444b5c578061c7efddbac5c82
Essonti <Strofim4enko@yandex.ru>
2022-05-11 23:28:11 +0300

    feat: implement cteating of new boards

--- project-management-app/src/components/Header/Header.tsx
+++ project-management-app/src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import './Header.css';
 import React, { FormEvent, useEffect, useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { IBoardForm } from '../../types/headerTypes';
+import axios from 'axios';
 
 export const Header = () => (
   <header className="app-header">
@@ -26,24 +27,21 @@ export const Header = () => (
   </header>
 );
 
+axios.defaults.headers.common['Authorization'] =
+  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIzYjNkNGEyNy1iMDg3LTRkM2QtOTM0OC0zZjg2ZmFhZmI2ZmEiLCJsb2dpbiI6InVzZXIxIiwiaWF0IjoxNjUyMTExMjY4fQ.EsmO7vXW5kUlyJjfy93YXYpB41p8z_AkQlqal1RGK6o';
+
 function RenderButton() {
   const [showModal, setShowModal] = useState(false);
 
-  const handleCreateBoard = () => {
+  const handleToggleModal = () => {
     showModal ? setShowModal(false) : setShowModal(true);
-    console.log(showModal);
   };
 
   return (
     <div className="create-board-wrapper">
-      <button
-        onClick={() => {
-          handleCreateBoard();
-        }}
-      >
-        Create
-      </button>
+      <button onClick={handleToggleModal}>Create</button>
       {showModal && <RenderModalCreateBoard />}
+      {showModal && <div className="overlay" onClick={handleToggleModal}></div>}
     </div>
   );
 }
@@ -52,11 +50,21 @@ function RenderModalCreateBoard() {
   const {
     register,
     handleSubmit,
-    watch,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm();
-  const onSubmit: SubmitHandler<IBoardForm> = (data) => console.log(data);
-  // console.log(watch('text'));
+  const onSubmit: SubmitHandler<IBoardForm> = (data) => {
+    console.log(data.title);
+    axios
+      .post('https://app-management-final.herokuapp.com/boards', {
+        title: data.title,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <form className="create-board-modal" onSubmit={handleSubmit(onSubmit)}>
@@ -66,12 +74,13 @@ function RenderModalCreateBoard() {
           className="create-board-modal__input"
           type="text"
           {...register('title', {
-            required: true,
+            required: 'Field has to be fulfilled',
           })}
         />
+        {errors.title && <p className="errors-message">{errors.title.message || 'error!'}</p>}
       </label>
+      <br />
+      <button>Create</button>
     </form>
   );
 }
-
-// : SubmitHandler<FieldValues>
